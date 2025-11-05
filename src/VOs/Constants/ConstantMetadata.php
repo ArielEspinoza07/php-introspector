d5c9c7b471d3527093b4c79dcb49dc99c8b28429
Ariel Espinoza <arielespinoza46@gmail.com>
2025-11-05 13:45:32 -0600

    chore: remove and add properties

--- src/VOs/Constants/ConstantMetadata.php
+++ src/VOs/Constants/ConstantMetadata.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Aurora\Reflection\VOs\Constants;
 
+use Aurora\Reflection\Enums\Visibility;
 use Aurora\Reflection\VOs\DocBlocks\DocBlockMetadata;
+use Aurora\Reflection\VOs\Types\TypeMetadata;
 use JsonSerializable;
 
 final readonly class ConstantMetadata implements JsonSerializable
@@ -12,10 +14,10 @@ final readonly class ConstantMetadata implements JsonSerializable
     public function __construct(
         public string $name,
         public mixed $value,
-        public bool $isPublic = true,
-        public bool $isProtected = false,
-        public bool $isPrivate = false,
+        public Visibility $visibility,
         public bool $isFinal = false,
+        public bool $isReadOnly = false,
+        public ?TypeMetadata $type = null,
         public ?DocBlockMetadata $docBlock = null,
     ) {}
 
@@ -27,10 +29,10 @@ final readonly class ConstantMetadata implements JsonSerializable
         return [
             'name' => $this->name,
             'value' => $this->value,
-            'is_public' => $this->isPublic,
-            'is_protected' => $this->isProtected,
-            'is_private' => $this->isPrivate,
+            'visibility' => $this->visibility,
             'is_final' => $this->isFinal,
+            'is_read_only' => $this->isReadOnly,
+            'type' => $this->type,
             'doc_block' => $this->docBlock?->toArray(),
         ];
     }
