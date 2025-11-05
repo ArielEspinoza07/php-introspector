068c673888df8438f8b2eddb80312c0d4350c0c5
Ariel Espinoza <arielespinoza46@gmail.com>
2025-11-04 23:33:57 -0600

    chore: add exception

---
+++ src/Exceptions/ReflectionReadException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Reflection\Exceptions;
+
+use ReflectionException;
+
+final class ReflectionReadException extends ReflectionException
+{
+    //
+}
