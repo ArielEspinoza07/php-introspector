d04f805771e4812704b4f22b30c920a3aed0a04f
Ariel Espinoza <arielespinoza46@gmail.com>
2025-11-05 00:19:49 -0600

    chore: add tags

---
+++ src/VOs/DocBlocks/ParamTag.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Reflection\VOs\DocBlocks;
+
+use JsonSerializable;
+
+final readonly class ParamTag implements JsonSerializable
+{
+    public function __construct(
+        public string $name,
+        public ?string $type = null,
+        public ?string $description = null,
+    ) {}
+
+    /**
+     * @return array<string, string|null>
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'type' => $this->type,
+            'description' => $this->description,
+        ];
+    }
+
+    /**
+     * @return array<string, string|null>
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+}
---
+++ src/VOs/DocBlocks/VarTag.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aurora\Reflection\VOs\DocBlocks;
+
+use JsonSerializable;
+
+final readonly class VarTag implements JsonSerializable
+{
+    public function __construct(
+        public ?string $type = null,
+        public ?string $description = null,
+    ) {}
+
+    /**
+     * @return array<string, string|null>
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => $this->type,
+            'description' => $this->description,
+        ];
+    }
+
+    /**
+     * @return array<string, string|null>
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+}
