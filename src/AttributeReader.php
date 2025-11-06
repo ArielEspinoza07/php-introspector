9cc0eefccff71a4771918f2fcf9eceb8f8b7774e
Ariel Espinoza <arielespinoza46@gmail.com>
2025-11-05 19:22:25 -0600

    chore: add methods

--- src/AttributeReader.php
+++ src/AttributeReader.php
@@ -6,6 +6,8 @@ namespace Aurora\Reflection;
 
 use Aurora\Reflection\VOs\Attributes\AttributeMetadata;
 use ReflectionAttribute;
+use ReflectionClass;
+use ReflectionException;
 
 /**
  * @template T of object
@@ -19,7 +21,37 @@ final class AttributeReader
     {
         return new AttributeMetadata(
             name: $attribute->getName(),
+            fqcn: $this->getFQCN($attribute),
+            namespace: $this->getNamespace($attribute),
             arguments: $attribute->getArguments(),
         );
+    }
+
+    /**
+     * @param  ReflectionAttribute<T>  $attribute
+     *
+     * @throws ReflectionException
+     */
+    private function getFQCN(ReflectionAttribute $attribute): string
+    {
+        $ref = new ReflectionClass($attribute->getName());
+
+        if (empty($ref->getNamespaceName())) {
+            return $ref->getNamespaceName().'\\'.$ref->getName();
+        }
+
+        return $ref->getName();
+    }
+
+    /**
+     * @param  ReflectionAttribute<T>  $attribute
+     *
+     * @throws ReflectionException
+     */
+    private function getNamespace(ReflectionAttribute $attribute): string
+    {
+        $ref = new ReflectionClass($attribute->getName());
+
+        return $ref->getNamespaceName();
     }
 }
